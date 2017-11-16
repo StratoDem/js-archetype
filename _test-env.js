87b5a4e5ec0ff5ecf0398c50f44e7ded16cb2241
michael <michael@stratodem.com>
2017-11-16 10:42:22 -0500

    Update test env file

--- _test-env.js
+++ _test-env.js
@@ -8,10 +8,10 @@
  *  All Rights Reserved
  */
 
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+const Enzyme = require('enzyme');
+const Adapter = require('enzyme-adapter-react-16');
 
-import $ from 'jquery';
+const $ = require('jquery');
 
 
 Enzyme.configure({ adapter: new Adapter() });
