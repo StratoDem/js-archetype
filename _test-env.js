1028efa386b0e0eb836bc31a488c8c5a65b14b3b
michael <michael@stratodem.com>
2017-11-16 10:40:03 -0500

    Update test env file

---
+++ _test-env.js
@@ -0,0 +1,20 @@
+/** @flow
+ * StratoDem Analytics : test-env
+ * Principal Author(s) : Michael Clawar
+ * Secondary Author(s) :
+ * Description :
+ *
+ *  (c) 2016- StratoDem Analytics, LLC
+ *  All Rights Reserved
+ */
+
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import $ from 'jquery';
+
+
+Enzyme.configure({ adapter: new Adapter() });
+
+global.$ = $;
+global.jQuery = $;
